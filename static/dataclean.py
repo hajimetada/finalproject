6f56aded274470b588b9a1eaf18769c0bc7c5c20
Hajime <Hajime@Hajimes-MacBook-Air.local>
2016-11-29 09:47:13 -0600

    final final

--- static/dataclean.py
+++ static/dataclean.py
@@ -4,15 +4,17 @@
 
 
 import pandas as pd
-# Read the csv file into df, using certain varibables.
-df_crime = pd.read_csv("Crimes_-_2001_to_present.csv", usecols = ["Primary Type", "Community Area", "Year"])
+# Read the csv file into dataframe.
+df_crime = pd.read_csv("Crimes_-_2001_to_present.csv")
+# Extract variables only necessary to this webapp.
+df_crime = df_crime[["Primary Type", "Community Area", "Year"]]
 # Extract the data of 2015 (2015 is the latest data which is complete),
 # and drop rows with missing values.
 df_crime = df_crime[df_crime["Year"]==2015].dropna()
 # Drop rows if "Primary Type" is "NON-CRIMINAL" or "NON - CRIMINAL",
 # since it's negligible information.
-mask = df_crime["Primary Type"]=="NON-CRIMINAL"
-mask &= df_crime["Primary Type"]=="NON - CRIMINAL"
+mask = df_crime["Primary Type"]!="NON-CRIMINAL"
+mask &= df_crime["Primary Type"]!="NON - CRIMINAL"
 df_crime = df_crime[mask]
 # Convert floats to integers.
 df_crime = df_crime.astype({"Primary Type":str, "Community Area":int, "Year":int})
